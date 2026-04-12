0ed38b35f274c9e4f91fe302ef017d1bbcf4ec16
98CharleS <karol.sojkowski@gmail.com>
2026-04-12 13:48:07 +0200

    Added tabels with division of tenders

--- tables_extractor.py
+++ tables_extractor.py
@@ -5,11 +5,13 @@ conn = sqlite3.connect("eZam_DB.db")
 
 tables = [
     "tenders_by_cpv",
+    "tenders_by_cpv_division",
     "tenders_by_gdp",
     "tenders_by_gdp_per_capita",
     "tenders_by_population",
     "tenders_by_population_warsaw_split",
     "tenders_by_province",
+    "tenders_by_province_and_division",
     "tenders_by_province_year",
     "top5_cpv_by_province",
     "top5_cpv_by_province_2021",
